550d3726d3b5fb9f4ad5cf57c966e15eb485ae48
Luis Rovirosa <luisrovirosa@gmail.com>
2014-06-25 20:36:49 +0200

    Refactoring

--- src/Minesweeper.php
+++ src/Minesweeper.php
@@ -5,6 +5,8 @@ namespace LuisRovirosa;
 class Minesweeper
 {
 
+    const BOMB = '*';
+
     private $board;
 
     public function myMethod()
@@ -15,7 +17,8 @@ class Minesweeper
     public function load($filename)
     {
         $fileContent = explode("\n", file_get_contents($filename));
-        list($numFiles, $numColumns) = explode(' ', $fileContent[0]);
+        $firstLine = array_shift($fileContent);
+        list($numFiles, $numColumns) = explode(' ', $firstLine);
         $this->board = array_fill(0, $numFiles, array_fill(0, $numColumns, '0'));
         $this->loadBombs($numFiles, $numColumns, $fileContent);
     }
@@ -25,15 +28,20 @@ class Minesweeper
         return $this->board;
     }
 
-    private function loadBombs($numFiles, $numColumns, $fileContent)
+    private function loadBombs($numFiles, $numColumns, $inputBoard)
     {
         for ($i = 0; $i < $numFiles; $i++) {
             for ($j = 0; $j < $numColumns; $j++) {
-                if ('*' == $fileContent[$i + 1][$j]) {
-                    $this->board[$i][$j] = '*';
-                    $this->increaseNumOfBombs($i, $j, $numFiles, $numColumns);
-                }
+                $this->processSquare($inputBoard[$i][$j], $i, $j, $numFiles, $numColumns);
             }
+        }
+    }
+
+    private function processSquare($square, $i, $j, $numFiles, $numColumns)
+    {
+        if ($this->isBomb($square)) {
+            $this->board[$i][$j] = self::BOMB;
+            $this->increaseNumOfBombs($i, $j, $numFiles, $numColumns);
         }
     }
 
@@ -41,11 +49,16 @@ class Minesweeper
     {
         for ($i = max(0, $file - 1); $i < min($numFiles, $file + 2); $i++) {
             for ($j = max(0, $column - 1); $j < min($numColumns, $column + 2); $j++) {
-                if ($this->board[$i][$j] != '*') {
+                if (!$this->isBomb($this->board[$i][$j])) {
                     $this->board[$i][$j] += 1;
                 }
             }
         }
+    }
+
+    private function isBomb($square)
+    {
+        return self::BOMB == $square;
     }
 
 }
